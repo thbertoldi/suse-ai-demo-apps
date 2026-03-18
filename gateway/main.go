0f74969e979dbe418ef881b81909bbaea824bd6b
Thiago Bertoldi <thiago.bertoldi@suse.com>
2026-03-18 16:16:58 -0300

    chore: update schema
    Signed-off-by: Thiago Bertoldi <thiago.bertoldi@suse.com>

--- gateway/main.go
+++ gateway/main.go
@@ -16,7 +16,7 @@ import (
 	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.40.0"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	healthgrpc "google.golang.org/grpc/health"
